af52205351ce4e2285e68f06a71ce94ac51df58b
YutaSugimura <nextc3m@gmail.com>
2021-05-20 23:08:49 +0900

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  require('../.mocks');
-}
-
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
