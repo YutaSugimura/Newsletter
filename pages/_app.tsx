fe48052b01e790df47822086c007658bf83cc9d4
YutaSugimura <nextc3m@gmail.com>
2021-05-22 19:50:12 +0900

    Add next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
